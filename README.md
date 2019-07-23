a37b1eb4f26966172aeb3a5793b6b1a4a70216c2
Stefanos Mandalas <sealmove@protonmail.com>
2019-07-23 15:48:31 +0300

    code re-architecturing

--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 
 ## What is xonsh?
 Xonsh is a relatively new shell (2016) and it's amazing in many ways. First and
-foremost it's an innovative shell language which combines the power of traditional
-shell language and the beauty of python. This project focuses on another cool
-feature though, the history system.
+foremost it's an innovative shell language which combines the power of
+traditional shell language and the beauty of python. This project focuses on
+another cool feature though, the history system.
 
 ## Xonsh history backend
 Xonsh's history system is plugable. There are already currently 3 available
 choices after installing (json, sqlite, dummy) and you can also write your own
-backend! This project uses the default backend (json), although it can be extended to support more.
+backend! This project uses the default backend (json), although it can be
+extended to support more.
 
 Xonsh logs commands *per session* in json files with a unique session id.
 Each session file includes roughly this information:
@@ -25,44 +26,67 @@ Each session file includes roughly this information:
 As you can imagine, xonsh's history system is increadibly powerful! It even
 provides a command, `history replay`, which can replay a session (kinda like
 asciinema and the likes). Sadly this is the only frontend feature that makes
-full use of the information in the json files. If you want to see any information
-about a specific command, all you can do is search the json files manually or
-replay the session.
+full use of the information in the json files. If you want to see any
+information about a specific command, all you can do is search the json files
+manually or replay the session.
 **tl;dr: Xonsh lacks a frontend tool for searching through the
 json files or producing traditional history log files**.
 
 ## What exactly does xhronicle do?
 Xhronicle is a simple cli with the following 3 subcommands:
 - print [-u] [-i] [-r] [-t] [-s] [since] [until]
-- output \<session id\> \<command id\> [--stripcolors]
+- output \<session id\> \<command index\> [--stripcolors]
 - enviroment \<session id\> [--json]
 
 You don't have to type the whole subcommand. For example `xhronicle p`,
 `xhronicle pri` and `xhronicle print` are all equivelant.
 
-### `print`:
+## `print`
 Prints a log file. If no option is provided, then only the
 commands are printed **in precise chronological order**, one line each.
-With options you can print more information in the line.
-None of the options take arguments and they are **order-sensitive**!
+With options you can print more information in the line or filter commands.
+It takes no arguments.
+
+### information
+None of these options take arguments and they are **order-sensitive**!
 
 | Short | Long | Meaning |
 |-------|------|----------
-| -u | --user | user who ran the command |
-| -i | --id | session id and command is |
-| -r | --returncode | return code of the command |
-| -s | --start | date and time when the command started execution |
-| -f | --finish | date and time when the command finished execution |
-| -d | --duration | duration of command execution in seconds |
-| -n | --nocolor | strips the output free of ascii color escape sequences |
+| -u | --user | user who ran the command
+| -i | --id | session id and command is
+| -r | --returncode | return code of the command
+| -b | --begin | date and time when the command beginned execution
+| -e | --end | date and time when the command ended execution
+| -d | --duration | duration of command execution in seconds
 
-### `output`
-Takes 2 mandatory arguments, a session id and a command id (collectively they map
-to a unique command), and writes to stdout what was written to stdout and stderr
-(by the command) when it was executed.
+### filtering
+All filtering commands require arguments.
 
-### `enviroment`
-Take 1 argument, a session id, and prints the *initial* enviroment variables of the corresponding session.
+| Short | Long | Argument(s) | Meaning
+|-------|------|---------- | -
+| -f | --from | date/time | only show commands ran after [date/time]
+| -t | --till | date/time | only show command
+| -s | --session | session id | only show commands ran during [session]
+
+There is also an option to strip colors. It accepts no arguments.
+
+| Short | Long | Meaning
+|-------|------|----------
+| -n | --nocolor | strips the output free of ascii color escape sequences
+
+
+## `output`
+Takes 3 mandatory arguments: user, session id, command index (collectively they
+map to a unique command); and writes to stdout what was written to stdout and
+stderr (by the command) when it was executed.
+
+## `enviroment`
+Takes 2 mandatory argument: user, session id; and prints the *initial* enviroment
+variables of the corresponding session.
+
+## `timestamps`
+Takes 2 mandatory argument: user, session id; and prints the timestamps of the
+session.
 
 ## How to generate binary
 * Clone
--- xhronicle.nim
+++ xhronicle.nim
@@ -1,80 +1,55 @@
-import parseopt, strutils, os, json, algorithm, times, math, strformat
+import parseopt, parseutils, strutils, os, json, algorithm, times, math,
+  strformat
 
-type
-  CmdInfo = tuple[ts: float, sid, cid: int]
+type CmdInfo = tuple
+  ts: float
+  sid, idx: int
 
 const
-  config = [
-    ("root", "/root/.local/share/xonsh"),
-    ("sealmove", "/home/sealmove/.local/share/xonsh")
+  xonfig = [
+    (name: "root", path: "/root/.local/share/xonsh"),
+    (name: "sealmove", path: "/home/sealmove/.local/share/xonsh")
   ]
-  defaultTimeFormat = "dd MMM YYYY hh:mm:ss"
+  defaultTimePrintFormat = "dd MMM YYYY hh:mm:ss"
+  defaultTimeParseFormat = "yyyy-M-d-h:m:s"
 
-var sessions: seq[tuple[user: string, data: JsonNode]]
-for entry in config:
-  for file in walkFiles(entry[1] / "*.json"):
-    try:
-      sessions.add((entry[0], parseFile(file)["data"]))
-    except:
-      discard # XXX: log error
+var
+  sessions: seq[tuple[user: string, data: JsonNode]]
+  commands: seq[CmdInfo]
 
-var commands: seq[CmdInfo]
-for i, session in sessions:
-  var j = 0 # XXX: find better way
-  for cmd in session[1]["cmds"]:
-    commands.add((getFloat(cmd["ts"][0]), i, j))
-    inc j
-
-sort(commands)
+proc initTime(time: JsonNode): Time =
+  let (sec, nanosec) = splitDecimal(getFloat(time))
+  result = initTime(int64(sec), int64(1_000_000_000 * nanosec))
 
 type
   FieldKind = enum
     fkInp
     fkOut
-    fkRtn
-    fkStrt
-    fkFin
-    fkDur
     fkEnv
     fkUser
     fkId
+    fkRtn
+    fkBeg
+    fkEnd
+    fkDur
   Field = ref FieldObj
   FieldObj = object
     case kind: FieldKind
     of fkInp, fkOut, fkUser, fkId: strVal: string
     of fkRtn: intVal: int
-    of fkStrt, fkFin: time: Time
+    of fkBeg, fkEnd: time: Time
     of fkDur: duration: float
     of fkEnv: env: JsonNode
 
-proc lookup(sid, cid: int, what: FieldKind): Field =
+proc lookup(sid, idx: int, what: FieldKind): Field =
   try:
     case what
     of fkInp:
-      let input = getStr(sessions[sid][1]["cmds"][cid]["inp"])
+      let input = getStr(sessions[sid][1]["cmds"][idx]["inp"])
       result = Field(kind: fkInp, strVal: input)
     of fkOut:
-      let output = getStr(sessions[sid][1]["cmds"][cid]["out"])
+      let output = getStr(sessions[sid][1]["cmds"][idx]["out"])
       result = Field(kind: fkInp, strVal: output)
-    of fkRtn:
-      let rtn = getInt(sessions[sid][1]["cmds"][cid]["rtn"])
-      result = Field(kind: fkRtn, intVal: rtn)
-    of fkStrt:
-      let
-        ts = sessions[sid][1]["cmds"][cid]["ts"]
-        (startSec, startNano) = splitDecimal(getFloat(ts[0]))
-        start = initTime(int64(startSec), int64(1_000_000_000 * startNano))
-      result = Field(kind: fkStrt, time: start)
-    of fkFin:
-      let
-        ts = sessions[sid][1]["cmds"][cid]["ts"]
-        (finishSec, finishNano) = splitDecimal(getFloat(ts[1]))
-        finish = initTime(int64(finishSec), int64(1_000_000_000 * finishNano))
-      result = Field(kind: fkFin, time: finish)
-    of fkDur:
-      let ts = sessions[sid][1]["cmds"][cid]["ts"]
-      let dur = getFloat(ts[1]) - getFloat(ts[0])
-      result = Field(kind: fkDur, duration: dur)
     of fkEnv:
       result = Field(kind: fkEnv, env: sessions[sid][1]["env"])
     of fkUser:
@@ -83,87 +58,224 @@ proc lookup(sid, cid: int, what: FieldKind): Field =
     of fkId:
       let id = getStr(sessions[sid][1]["sessionid"])
       result = Field(kind: fkId, strVal: id)
+    of fkRtn:
+      let rtn = getInt(sessions[sid][1]["cmds"][idx]["rtn"])
+      result = Field(kind: fkRtn, intVal: rtn)
+    of fkBeg:
+      let ts = sessions[sid][1]["cmds"][idx]["ts"]
+      result = Field(kind: fkBeg, time: initTime(ts[0]))
+    of fkEnd:
+      let ts = sessions[sid][1]["cmds"][idx]["ts"]
+      result = Field(kind: fkEnd, time: initTime(ts[1]))
+    of fkDur:
+      let ts = sessions[sid][1]["cmds"][idx]["ts"]
+      let dur = getFloat(ts[1]) - getFloat(ts[0])
+      result = Field(kind: fkDur, duration: dur)
   except:
     discard # XXX: log error
 
-if paramCount() == 0:
-  quit QuitFailure
-
-type OrderedOption = enum
-  ooUser
-  ooId
-  ooRtn
-  ooStrt
-  ooFin
-  ooDur
-
 if "print".startsWith(paramStr(1)):
+  type
+    Config = tuple
+      # Filter options
+      session: tuple[flag: bool, sid: string]
+      fromm, till: Time
+      # Print options (ordered)
+      print: seq[tuple[order: int, option: PrintOption]]
+      # Other options
+      strip: bool
+    PrintOption = enum
+      poUser
+      poId
+      poRtn
+      poBeg
+      poEnd
+      poDur
+
+  const
+    shorts = {'u', 'i', 'r', 'b', 'e', 'd', 'n'}
+    longs = @["user", "id", "returncode", "begin", "end", "duration",
+      "nocolor"]
   var
     isFirstIter = true
-    orderedOpts: seq[tuple[order: int, option: OrderedOption]]
-    optStrip: bool
     i: int
-  const
-    shorts = {'u', 'i', 'r', 's', 'f', 'd', 'n'}
-    longs = @["user", "id", "returncode", "nocolor", "start", "finish",
-      "duration"]
+    optConfig: Config
+  
+  optConfig.till = fromUnix(int64.high)
+
   for kind, key, val in getopt(shortNoVal = shorts, longNoVal = longs):
     if isFirstIter:
       isFirstIter = false
       continue
     case kind
-    of cmdArgument:
-      discard
     of cmdShortOption, cmdLongOption:
       case key
+      of "session", "s":
+        optConfig.session = (true, val)
+      of "from", "f":
+        optConfig.fromm = parseTime(val, defaultTimeParseFormat, local())
+      of "till", "t":
+        optConfig.till = parseTime(val, defaultTimeParseFormat, local())
       of "user", "u":
-        orderedOpts.add((i, ooUser))
+        optConfig.print.add((i, poUser))
       of "id", "i":
-        orderedOpts.add((i, ooId))
+        optConfig.print.add((i, poId))
       of "returncode", "r":
-        orderedOpts.add((i, ooRtn))
-      of "start", "s":
-        orderedOpts.add((i, ooStrt))
-      of "finish", "f":
-        orderedOpts.add((i, ooFin))
+        optConfig.print.add((i, poRtn))
+      of "begin", "b":
+        optConfig.print.add((i, poBeg))
+      of "end", "e":
+        optConfig.print.add((i, poEnd))
       of "duration", "d":
-        orderedOpts.add((i, ooDur))
+        optConfig.print.add((i, poDur))
       of "nocolor", "n":
-        optStrip = true
+        optConfig.strip = true
       else:
         echo "Error while parsing option"
-        quit 0
+        quit QuitFailure
+    of cmdArgument:
+      echo "This command does not accept arguments"
+      quit QuitFailure
     of cmdEnd:
       assert(false)
     inc i
-  sort(orderedOpts)
+  
+  sort(optConfig.print)
+
+  for entry in xonfig:
+    if optConfig.session.flag:
+      try:
+        let file = entry.path / "xonsh-" & optConfig.session.sid & ".json"
+        sessions.add((entry.name, parseFile(file)["data"]))
+      except:
+        discard # XXX: log error
+    else:
+      for file in walkFiles(entry.path / "*.json"):
+        try:
+          sessions.add((entry.name, parseFile(file)["data"]))
+        except:
+          discard # XXX: log error
+
+  for i, session in sessions:
+    var j = 0
+    for cmd in session.data["cmds"]:
+      if getFloat(cmd["ts"][0]).int64 > optConfig.fromm.toUnix and
+         getFloat(cmd["ts"][0]).int64 < optConfig.till.toUnix:
+        commands.add((getFloat(cmd["ts"][0]), i, j))
+        inc j
+
+  sort(commands)
+
   for cmd in commands:
     var str: string
-    for oo in orderedOpts:
-      case oo[1]
-      of ooUser:
-        str &= lookup(cmd[1], cmd[2], fkUser).strVal & "\t"
-      of ooId:
-        str &= lookup(cmd[1], cmd[2], fkId).strVal & "[" & $cmd[2] & "]\t"
-      of ooRtn:
-        str &= $lookup(cmd[1], cmd[2], fkRtn).intVal & "\t"
-      of ooStrt:
-        let start = lookup(cmd[1], cmd[2], fkStrt).time
-        str &= start.format(defaultTimeFormat) & "\t"
-      of ooFin:
-        let finish = lookup(cmd[1], cmd[2], fkFin).time
-        str &= finish.format(defaultTimeFormat) & "\t"
-      of ooDur:
+    for p in optConfig.print:
+      case p.option
+      of poUser:
+        str &= lookup(cmd.sid, cmd.idx, fkUser).strVal & "\t"
+      of poId:
+        str &= lookup(cmd.sid, cmd.idx, fkId).strVal & "[" & &"{cmd.idx:03}" & "]\t"
+      of poRtn:
+        str &= $lookup(cmd.sid, cmd.idx, fkRtn).intVal & "\t"
+      of poBeg:
+        let beg = lookup(cmd.sid, cmd.idx, fkBeg).time
+        str &= beg.format(defaultTimePrintFormat) & "\t"
+      of poEnd:
+        let endd = lookup(cmd.sid, cmd.idx, fkEnd).time
+        str &= endd.format(defaultTimePrintFormat) & "\t"
+      of poDur:
         let
-          dur = lookup(cmd[1], cmd[2], fkDur).duration
+          dur = lookup(cmd.sid, cmd.idx, fkDur).duration
         str &= &"{dur:>7.2f}" & "s\t"
-    stdout.write(str & lookup(cmd[1], cmd[2], fkInp).strVal)
-elif "returncode".startsWith(paramStr(1)):
-  if paramCount() != 3:
-    quit QuitFailure
+    stdout.write(str & lookup(cmd.sid, cmd.idx, fkInp).strVal)
+
 elif "output".startsWith(paramStr(1)):
-  discard # XXX
+  if paramCount() != 4:
+    echo "This commands takes exactly 3 arguments"
+    quit QuitFailure
+  # Find user's history directory
+  var path: string
+  for x in xonfig:
+    if x.name == paramStr(2):
+      path = x.path
+      break
+  if path == "":
+    echo "User not found"
+    quit QuitFailure
+  let file = path / "xonsh-" & paramStr(3) & ".json"
+  var
+    command: JsonNode
+    output: string
+    idx: int
+  try:
+    idx = parseInt(paramStr(4))
+  except ValueError:
+    echo "Could not parse command index"
+    quit QuitFailure
+  try:
+    command = parseFile(file)["data"]["cmds"][idx]
+  except:
+    echo "Json error"
+    quit QuitFailure
+  try:
+    output = getStr(command["out"])
+  except KeyError:
+    echo "Output of command not found (maybe output logging was not " &
+      "enabled in xonsh at the time?)"
+    quit QuitFailure
+  echo output
+
 elif "enviroment".startsWith(paramStr(1)):
-  discard # XXX
+  if paramCount() != 3:
+    echo "This commands takes exactly 2 arguments"
+    quit QuitFailure
+  # Find user's history directory
+  var path: string
+  for x in xonfig:
+    if x.name == paramStr(2):
+      path = x.path
+      break
+  if path == "":
+    echo "User not found"
+    quit QuitFailure
+  let file = path / "xonsh-" & paramStr(3) & ".json"
+  var enviroment: JsonNode
+  try:
+    enviroment = parseFile(file)["data"]["env"]
+  except:
+    echo "Json error"
+    quit QuitFailure
+  echo pretty(enviroment)
+
+elif "timestamps".startsWith(paramStr(1)):
+  if paramCount() != 3:
+    echo "This commands takes exactly 2 arguments"
+    quit QuitFailure
+  # Find user's history directory
+  var path: string
+  for x in xonfig:
+    if x.name == paramStr(2):
+      path = x.path
+      break
+  if path == "":
+    echo "User not found"
+    quit QuitFailure
+  let file = path / "xonsh-" & paramStr(3) & ".json"
+  var ts: JsonNode
+  try:
+    ts = parseFile(file)["data"]["ts"]
+  except:
+    echo "Json error"
+    quit QuitFailure
+  let
+    beg = initTime(ts[0])
+    endd = initTime(ts[1])
+    dur = getFloat(ts[1]) - getFloat(ts[0])
+  var str: string
+  str &= beg.format(defaultTimePrintFormat) & "\t"
+  str &= endd.format(defaultTimePrintFormat) & "\t"
+  str &= &"({dur:.2f}" & "s)\t"
+  echo str
+
 else:
-  discard # XXX: print error command not found
+  echo "Command not found"
+  quit QuitFailure
